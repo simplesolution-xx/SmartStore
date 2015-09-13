dcbacb6460c4d425b034df17b9316d5a380ff05e
Chittapol.Dai <Chittapol.Dai1990@gmail.com>
2015-09-13 18:50:43 +0700

    Test commit

--- src/com/smartstore/core/controller/HomeController.java
+++ src/com/smartstore/core/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String viewHome(){
-        System.out.println("Home controller!");
+        System.out.println("Home controller LOL!");
         return "index";
     }
 }
