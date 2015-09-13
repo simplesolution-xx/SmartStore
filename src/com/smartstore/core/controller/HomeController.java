18baf7e0441d64bf258c2a2b9fdc272b2017dba5
TOP <i_top@msn.com>
2015-09-13 18:19:32 +0700

    Initial commit

---
+++ src/com/smartstore/core/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.smartstore.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by TOP on 9/13/2015.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String viewHome(){
+        System.out.println("Home controller!");
+        return "index";
+    }
+}
