528dfaab298424d3bf5cd4fd3877664eda112ffc
TOP <i_top@msn.com>
2015-09-13 21:55:58 +0700

    Hi

--- src/com/smartstore/core/controller/HomeController.java
+++ src/com/smartstore/core/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String viewHome(){
-        System.out.println("Home controller LOL!");
+        System.out.println("Home controller LOL2!");
         return "index";
     }
 }
